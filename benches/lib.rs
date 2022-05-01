fa6650a69ec19d0b5b12c1d77c3f54627756c906
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-04-30 23:18:55 -0700

    Add a long benchmark test (#6)

--- benches/lib.rs
+++ benches/lib.rs
@@ -6,3 +6,12 @@ extern crate test;
 fn bench_valid(b: &mut test::Bencher) {
     b.iter(|| luhnmod10::valid("4242424242424242"));
 }
+
+#[bench]
+fn bench_valid_long(b: &mut test::Bencher) {
+    b.iter(|| {
+        luhnmod10::valid(
+            "42424242424242424242424242424242424242424242424242424242424242424242424242424240",
+        )
+    });
+}
